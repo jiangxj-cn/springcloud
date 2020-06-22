0e4a13e8a7bc3a61133abf97306331760b882aba
jiangxj <jiangxj@softbrain-offshore.com.cn>
2020-06-22 18:37:06 +0800

    Eureka add
    Eureka add

--- microservicecloud/microservicecloud-provider-dept/src/main/java/com/jiang/springcloud/Microservicecloud8001_app.java
+++ microservicecloud/microservicecloud-provider-dept/src/main/java/com/jiang/springcloud/Microservicecloud8001_app.java
@@ -2,8 +2,12 @@ package com.jiang.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class Microservicecloud8001_app {
     public static void main(String[] args) {
     SpringApplication.run(Microservicecloud8001_app.class, args);
--- microservicecloud/microservicecloud-provider-dept/src/main/java/com/jiang/springcloud/controller/DeptController.java
+++ microservicecloud/microservicecloud-provider-dept/src/main/java/com/jiang/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.jiang.springcloud.controller;
 import com.jiang.entities.Dept;
 import com.jiang.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +14,9 @@ public class DeptController {
 
     @Autowired
     private DeptService service;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping(value="/")
     public String hello(){
@@ -31,5 +36,16 @@ public class DeptController {
     @RequestMapping(value="/dept/list",method = RequestMethod.GET)
     public List<Dept> list(){
         return service.list();
+    }
+
+    @RequestMapping(value="/dept/discovery",method = RequestMethod.GET)
+    public Object discovery() {
+        List<String> list = discoveryClient.getServices();
+        System.out.println("**************" + list);
+        List<ServiceInstance> srvList = discoveryClient.getInstances("MICROSERVICECLOUD-DEPT");
+        for (ServiceInstance serviceInstance : srvList) {
+            System.out.println(serviceInstance.toString());
+        }
+        return this.discoveryClient;
     }
 }
